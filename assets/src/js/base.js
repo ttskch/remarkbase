01d7b1c1017dfd515bced9488cf2266474b02792
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2016-10-11 00:50:04 +0900

    [bug fix] support remark.js v1.14.0 (see https://github.com/gnab/remark/commit/760b0ba62ec37f65da04e49fb00260fad73eb66d)

--- assets/src/js/base.js
+++ assets/src/js/base.js
@@ -1,17 +1,19 @@
-$(function () {
-    emojify.setConfig({
-        img_dir: '//rawgithub.com/Ranks/emojify.js/master/dist/images/basic/',
-        ignore_emoticons: true
-    });
-    emojify.run();
+var slideshowCallback = function () {
+    $(function () {
+        emojify.setConfig({
+            img_dir: '//cdnjs.cloudflare.com/ajax/libs/emojify.js/1.1.0/images/basic/',
+            ignore_emoticons: true
+        });
+        emojify.run();
 
-    $('[class*="font-"]').each(function () {
-        var ratio = $(this).attr('class').replace(/^.*font-(\d+).*$/, '$1');
-        $(this).css('font-size', ratio + '%');
-    });
+        $('[class*="font-"]').each(function () {
+            var ratio = $(this).attr('class').replace(/^.*font-(\d+).*$/, '$1');
+            $(this).css('font-size', ratio + '%');
+        });
 
-    $('[class*="width-"]').each(function () {
-        var width = $(this).attr('class').replace(/^.*width-(\d+).*$/, '$1');
-        $(this).find('> *').css('width', width + '%');
+        $('[class*="width-"]').each(function () {
+            var width = $(this).attr('class').replace(/^.*width-(\d+).*$/, '$1');
+            $(this).find('> *').css('width', width + '%');
+        });
     });
-});
+};
